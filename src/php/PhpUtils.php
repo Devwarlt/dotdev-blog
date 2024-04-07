65f2ed08211c32d0d150bdf6a879660cc0fc2a7e
Devwarlt <nadio.pontes@redes.unb.br>
2024-04-07 18:27:53 -0300

    Create PhpUtils.php

---
+++ src/php/PhpUtils.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace php;
+
+final class PhpUtils
+{
+    public static PhpUtils $singleton;
+    private static string $php_injection_regex_pattern = '/^(?=.*<\?)|(?=.*\?>).*$/';
+
+    private function __construct()
+    {
+    }
+
+    public static function getSingleton(): PhpUtils
+    {
+        if (self::$singleton === null) self::$singleton = new PhpUtils();
+        return self::$singleton;
+    }
+
+    public function onRawIndexErr(string $msg, string $ref): void
+    {
+        self::onRawRedirect($msg, $ref, "err");
+    }
+
+    private function onRawRedirect(string $msg, string $ref, string $var): void
+    {
+        $val = urlencode($msg);
+        header("Location:$ref?$var=$val");
+    }
+
+    public function onRawIndexOk(string $msg, string $ref): void
+    {
+        self::onRawRedirect($msg, $ref, "ok");
+    }
+
+    public function checkPhpInjection(string $str): bool
+    {
+        return preg_match(self::$php_injection_regex_pattern, $str);
+    }
+
+    public function getContents(string $path): string
+    {
+        $file = dirname(__FILE__) . $path;
+        if (!file_exists($file)) return "<p style='color: red'><strong>Arquivo não encontrado:</strong> " . dirname(__FILE__) . "$path</p>";
+
+        return file_get_contents($file);
+    }
+}
