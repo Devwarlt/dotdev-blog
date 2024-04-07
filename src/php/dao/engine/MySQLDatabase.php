45f9a3e5cb3705bf41450206c361da148a04a62c
Devwarlt <nadio.pontes@redes.unb.br>
2024-04-07 20:28:23 -0300

    Update MySQLDatabase.php

--- src/php/dao/engine/MySQLDatabase.php
+++ src/php/dao/engine/MySQLDatabase.php
@@ -15,27 +15,28 @@ final class MySQLDatabase
     private static PDO $connection;
     private static MySQLDatabase $singleton;
 
-    private function __construct(PDO $connection)
-    {
-        self::$connection = $connection;
-    }
+    private function __construct(PDO $connection) { self::$connection = $connection; }
 
     public static function getSingleton(): MySQLDatabase
     {
-        if (self::$singleton === null) self::$singleton = new MySQLDatabase(new PDO("engine:host=" . DB_HOST . ";dbname=" . DB_SCHEMA, DB_USER, DB_PASSWORD));
-
+        if (self::$singleton === null)
+            self::$singleton = new MySQLDatabase(
+                new PDO(
+                    "engine:host=" . DB_HOST . ";dbname=" . DB_SCHEMA,
+                    DB_USER,
+                    DB_PASSWORD
+                )
+            );
         return self::$singleton;
     }
 
-    public function update(SQLQuery $query): bool
-    {
-        return $this->dml($query->getQuery());
-    }
+    public function update(SQLQuery $query): bool { return $this->dml($query->getQuery()); }
 
     private function dml(string $sql): bool
     {
         $query = self::$connection->prepare($sql);
-        if ($query->execute()) return true; else {
+        if ($query->execute()) return true;
+        else {
             $this->displayPDOError($query);
             return false;
         }
@@ -45,7 +46,10 @@ final class MySQLDatabase
     {
         echo "
         <div class='card text-white bg-danger' style='padding: 4px; width: 100%'>
-            <div class='text-warning card-header'><strong><span class='glyphicon glyphicon-alert' aria-hidden='true'></span>&nbsp;&nbsp;<u>Query error detected!</u></strong></div>
+            <div class='text-warning card-header'>
+                <strong><span class='glyphicon glyphicon-alert' aria-hidden='true'></span>
+                &nbsp;&nbsp;
+                <u>Query error detected!</u></strong></div>
             <div class='card-body'>
                 <h5>Query:</h5>&nbsp;
                 <code class='text-warning bg-dark border-warning rounded'>&nbsp;" . $query->queryString . "&nbsp;</code>            
@@ -57,30 +61,19 @@ final class MySQLDatabase
                 <p><small><strong>&blacktriangleright;&nbsp;Driver-specific Error Message:</strong> " . $query->errorInfo()[2] . "</small></p>
             </div>
         </div>
-        <hr/>
-        ";
+        <hr/>";
     }
 
-    public function delete(SQLQuery $query): bool
-    {
-        return $this->dml($query->getQuery());
-    }
+    public function delete(SQLQuery $query): bool { return $this->dml($query->getQuery()); }
 
-    public function insert(SQLQuery $query): bool
-    {
-        return $this->dml($query->getQuery());
-    }
+    public function insert(SQLQuery $query): bool { return $this->dml($query->getQuery()); }
 
-    public function select(SQLQuery $query): ?PDOStatement
-    {
-        return $this->dql($query->getQuery());
-    }
+    public function select(SQLQuery $query): ?PDOStatement { return $this->dql($query->getQuery()); }
 
     private function dql(string $sql): ?PDOStatement
     {
         $query = self::$connection->query($sql);
         if (($success = $query->execute()) && $query->rowCount() > 0) return $query;
-
         if (!$success) $this->displayPDOError($query);
         return null;
     }
