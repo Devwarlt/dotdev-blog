7eb9df88291794dbc5bd12988ddfb7a231b2b520
Devwarlt <nadio.pontes@redes.unb.br>
2024-04-07 21:16:12 -0300

    Create LoginResultModel.php

---
+++ src/php/model/LoginResultModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace php\model;
+
+final class LoginResultModel
+{
+    private ?LoginModel $login;
+    private string $err;
+    private bool $status = false;
+
+    public function __construct() { }
+
+    public function getLogin(): ?LoginModel { return $this->login; }
+
+    public function getErr(): string { return $this->err; }
+
+    public function getStatus() : bool { return $this->status; }
+
+    public function setLogin(?LoginModel $login) : void { $this->login = $login; }
+
+    public function setErr(string $err) : void { $this->err = $err; }
+
+    public function setStatus(bool $status) : void { $this->status = $status; }
+}
