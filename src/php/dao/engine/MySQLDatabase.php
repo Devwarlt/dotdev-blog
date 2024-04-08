e8ae032a456ba7105e4d7260afe90a903f7bab09
Devwarlt <nadio.pontes@redes.unb.br>
2024-04-07 22:36:09 -0300

    WIP PDO connection to MySQL

--- src/php/dao/engine/MySQLDatabase.php
+++ src/php/dao/engine/MySQLDatabase.php
@@ -3,6 +3,7 @@
 namespace php\dao\db;
 
 use PDO;
+use PDOException;
 use PDOStatement;
 
 define("DB_HOST", "localhost");
@@ -13,21 +14,24 @@ define("DB_PASSWORD", hash('sha256', 'toor'));
 final class MySQLDatabase
 {
     private static PDO $connection;
-    private static MySQLDatabase $singleton;
+    private static $singleton;
 
     private function __construct(PDO $connection) { self::$connection = $connection; }
 
     public static function getSingleton(): MySQLDatabase
     {
-        if (self::$singleton === null)
-            self::$singleton = new MySQLDatabase(
-                new PDO(
-                    "engine:host=" . DB_HOST . ";dbname=" . DB_SCHEMA,
-                    DB_USER,
-                    DB_PASSWORD
-                )
-            );
-        return self::$singleton;
+        try {
+            if (self::$singleton === null)
+                self::$singleton = new MySQLDatabase(
+                    new PDO(
+                        "engine:host=" . DB_HOST . ";dbname=" . DB_SCHEMA,
+                        DB_USER,
+                        DB_PASSWORD
+                    )
+                );
+            return self::$singleton;
+        }
+        catch (PDOException $e) { self::displayPDOException($e); }
     }
 
     public function update(SQLQuery $query): bool { return $this->dml($query->getQuery()); }
@@ -37,31 +41,76 @@ final class MySQLDatabase
         $query = self::$connection->prepare($sql);
         if ($query->execute()) return true;
         else {
-            $this->displayPDOError($query);
+            self::displayPDOError($query);
             return false;
         }
     }
 
-    private function displayPDOError(PDOStatement $query): void
+    private static function displayPDOError(PDOStatement $query): void
     {
         echo "
-        <div class='card text-white bg-danger' style='padding: 4px; width: 100%'>
-            <div class='text-warning card-header'>
-                <strong><span class='glyphicon glyphicon-alert' aria-hidden='true'></span>
-                &nbsp;&nbsp;
-                <u>Query error detected!</u></strong></div>
-            <div class='card-body'>
-                <h5>Query:</h5>&nbsp;
-                <code class='text-warning bg-dark border-warning rounded'>&nbsp;" . $query->queryString . "&nbsp;</code>            
-            </div>
-            <div class='card-footer'>
-                <h5>PDO::errorInfo():</h5>
-                <p><small><strong>&blacktriangleright;&nbsp;SQLSTATE Error Code:</strong> " . $query->errorInfo()[0] . "</small></p>
-                <p><small><strong>&blacktriangleright;&nbsp;Driver-specific Error Code:</strong> " . $query->errorInfo()[1] . "</small></p>
-                <p><small><strong>&blacktriangleright;&nbsp;Driver-specific Error Message:</strong> " . $query->errorInfo()[2] . "</small></p>
-            </div>
+<!DOCTYPE html>
+<html lang='pt' style='height: 100%;'>
+<head>
+    <meta charset='UTF-8''>
+    <title>.DEV Blog - Error</title>
+    <link rel='stylesheet' href='../../../css/bootstrap.min.css'/>
+</head>
+<body style='height: 100%;' class='h-100 row align-items-center  alert-light'>
+    <div class='card text-white bg-danger' style='padding: 4px; width: 100%'>
+        <div class='text-warning card-header'>
+            <strong><span class='glyphicon glyphicon-alert' aria-hidden='true'></span>&nbsp;&nbsp;
+            <u>Query error detected!</u></strong></div>
+        <div class='card-body'>
+            <h5>Query:</h5>&nbsp;
+            <pre class='text-warning bg-dark border-warning rounded'>
+                &nbsp;" . $query->queryString . "&nbsp;
+            </pre>            
         </div>
-        <hr/>";
+        <div class='card-footer'>
+            <h5>PDO::errorInfo():</h5>
+            <p><small><strong>&blacktriangleright;&nbsp;SQLSTATE Error Code:</strong> "
+                . $query->errorInfo()[0] . "</small></p>
+            <p><small><strong>&blacktriangleright;&nbsp;Driver-specific Error Code:</strong> "
+                . $query->errorInfo()[1] . "</small></p>
+            <p><small><strong>&blacktriangleright;&nbsp;Driver-specific Error Message:</strong> "
+                . $query->errorInfo()[2] . "</small></p>
+        </div>
+    </div>
+    <hr/>
+    <script type='text/javascript' src='../../../js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='../../../js/bootstrap.bundle.min.js'></script>
+</body>
+</html>";
+    }
+
+    private static function displayPDOException(PDOException $exception): void
+    {
+        echo "
+<!DOCTYPE html>
+<html lang='pt' style='height: 100%;'>
+<head>
+    <meta charset='UTF-8''>
+    <title>.DEV Blog - Error</title>
+    <link rel='stylesheet' href='../../../css/bootstrap.min.css'/>
+</head>
+<body style='height: 100%;' class='h-100 row align-items-center  alert-light'>
+    <div class='card text-white bg-danger' style='padding: 4px; width: 100%'>
+        <div class='text-warning card-header'>
+            <strong><span class='glyphicon glyphicon-alert' aria-hidden='true'></span>&nbsp;&nbsp;
+            <u>Internal connection exception!</u></strong></div>
+        <div class='card-body'>
+            <h5>Stack trace:</h5>
+            <pre class='text-warning bg-dark border-warning rounded'>"
+            . json_encode($exception->getTrace(), JSON_PRETTY_PRINT) .
+            "</pre>
+        </div>
+    </div>
+    <hr/>
+    <script type='text/javascript' src='../../../js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='../../../js/bootstrap.bundle.min.js'></script>
+</body>
+</html>";
     }
 
     public function delete(SQLQuery $query): bool { return $this->dml($query->getQuery()); }
