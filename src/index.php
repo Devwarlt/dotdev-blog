4b04ff4d65684dd3e168da75e17aaf535c427f4e
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-25 23:50:22 -0300

    WIP POST to POST redirects via HTML header

--- src/index.php
+++ src/index.php
@@ -7,119 +7,97 @@
     <link rel="stylesheet" href="css/custom.css?t=<?php echo time(); ?>"/>
 </head>
 <body class="bg-image">
-    <header class="navbar navbar-dark flex-column flex-md-row bg-dark shadow-lg
+<header class="navbar navbar-dark flex-column flex-md-row bg-dark shadow-lg
         opacity-75 border-secondary-subtle border-bottom border-0 border-opacity-10">
-        <a class="navbar-brand ms-md-0 ms-md-5" href="/"><strong>.DEV Blog</strong></a>
-        <div class="navbar-nav-scroll navbar-expand">
-            <ul class="navbar-nav flex-md-row">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Postagens</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="/">Ranking</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Editores</a>
-                </li>
-            </ul>
+    <a class="navbar-brand ms-md-0 ms-md-5" href="/"><strong>.DEV Blog</strong></a>
+    <div class="navbar-nav-scroll navbar-expand">
+        <ul class="navbar-nav flex-md-row">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Postagens</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/">Ranking</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Editores</a>
+            </li>
+        </ul>
+    </div>
+    <div class="float-end my-lg-auto text-light">
+        <a class="btn btn-outline-success ms-md-0 ms-md-2" href="/login">
+            <strong>Entrar</strong>
+        </a>
+        <a class="btn btn-outline-secondary ms-md-0 ms-md-2" href="/">
+            <strong>Cadastrar</strong>
+        </a>
+    </div>
+</header>
+<div class="container">
+    <div class="d-flex mt-5 justify-content-center">
+        <div class="row align-self-center w-50">
+            <?php
+
+            require("php\PhpUtils.php");
+            require("php\controller\LoginController.php");
+
+            use php\PhpUtils as utils;
+
+            if (isset($_POST["err"])) {
+                $err = urldecode($_POST["err"]);
+                if (utils::getSingleton()->checkPhpInjection($err)) {
+                    utils::getSingleton()->onRedirectErr("Php Injection detectado!", "/");
+                    return;
+                }
+
+                echo "
+                <div class='form-group col mt-0'>
+                    <div class='d-flex justify-content-center small'>
+                        <div class='alert small alert-warning border-warning alert-dismissible fade show col-sm-12'
+                             style='text-justify: inter-word; text-align: justify' role='alert'>
+                            <p class='mb-0'>$err</p>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'>
+                            </button>
+                            <hr/>
+                            <p class='mb-0'>
+                                <span class='glyphicon glyphicon-question-sign'></span>
+                                Não possui cadastro? <a class='alert-link' href='/register'>Clique aqui!</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class='small d-flex justify-content-center'>
+                    </div>
+                </div>";
+            }
+            ?>
         </div>
-        <div class="float-end my-lg-auto text-light">
-            <a class="btn btn-outline-success ms-md-0 ms-md-2" href="/login">
-                <strong>Entrar</strong>
-            </a>
-            <a class="btn btn-outline-secondary ms-md-0 ms-md-2" href="/">
-                <strong>Cadastrar</strong>
-            </a>
-        </div>
-    </header>
-    <div class="container">
-        <div class="d-flex mt-5 justify-content-center">
-            <div class="row align-self-center w-50">
-    <?php
-
-    include "php/controller/LoginController.php";
-
-    use php\controller\LoginController;
-    use php\PhpUtils as phputils;
-
-    session_start();
-
-    $redirect = "/";
-    $login = LoginController::getSingleton();
-    if ($login->isUserSignedUp()) {
-        header("Location:/account");
-        return;
-    }
-
-    if (isset($_GET["err"])) {
-        include "php/PhpUtils.php";
-
-        $utils = phputils::getSingleton();
-        $err = urldecode($_GET["err"]);
-        if ($utils->checkPhpInjection($err)) {
-            $utils->onRawIndexErr("Php Injection detectado!", $redirect);
-            return;
-        }
-
-        echo "
-                <div class='alert alert-danger border-danger alert-dismissible fade show top-50' role='alert'>
-                    <h3 style='color: darkred'>Erro!</h3>
-                    <hr/>
-                    <p style='color: red; text-align: justify; text-justify: inter-word'>$err</p>
-                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'></button>
-                </div>
-        ";
-    }
-
-    if (isset($_GET["ok"])) {
-        include "php/PhpUtils.php";
-
-        $utils = phputils::getSingleton();
-        $ok = urldecode($_GET["ok"]);
-        if ($utils->checkPhpInjection($ok)) {
-            $utils->onRawIndexErr("Php Injection detectado!", $redirect);
-            return;
-        }
-
-        echo "
-                <div class='alert alert-success border-success alert-dismissible fade show top-50' role='alert'>
-                    <h3 style='color: green'>Sucesso!</h3>
-                    <hr/>
-                    <p style='color: limegreen; text-align: justify; text-justify: inter-word'>$ok</p>
-                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'></button>
-                </div>
-        ";
-    }
-    ?>
+    </div>
+</div>
+<footer class="card-footer py-3 mt-auto bg-body-secondary fixed-bottom border-success-subtle border-top border-5">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-lg-5 mb-0">
+                <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/"
+                   aria-label=".DEV Blog">
+                    <span class="fs-5">.DEV Blog</span>
+                </a>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2">
+                        <strong>Projeto Integrado III</strong>
+                    </li>
+                    <li class="mb-2">
+                        <strong>Aluno:</strong> Nádio Dib Fernandes Pontes [matrícula: <code>201918579</code>]
+                    </li>
+                    <li class="mb-2">
+                        Código licenciado pelo
+                        <a class="alert-link"
+                           href="https://github.com/Devwarlt/dotdev-blog#MIT-1-ov-file">MIT</a>.
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <footer class="card-footer py-3 mt-auto bg-body-secondary fixed-bottom border-success-subtle border-top border-5">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-lg-5 mb-0">
-                    <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/"
-                        aria-label=".DEV Blog">
-                        <span class="fs-5">.DEV Blog</span>
-                    </a>
-                    <ul class="list-unstyled small text-muted">
-                        <li class="mb-2">
-                            <strong>Projeto Integrado III</strong>
-                        </li>
-                        <li class="mb-2">
-                            <strong>Aluno:</strong> Nádio Dib Fernandes Pontes [matrícula: <code>201918579</code>]
-                        </li>
-                        <li class="mb-2">
-                            Código licenciado pelo
-                            <a class="alert-link"
-                               href="https://github.com/Devwarlt/dotdev-blog#MIT-1-ov-file">MIT</a>.
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+</footer>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- src/login.php
+++ src/login.php
@@ -8,143 +8,140 @@
     <link rel="stylesheet" href="css/custom.css?t=<?php echo time(); ?>"/>
 </head>
 <body class="bg-image">
-    <header class="navbar navbar-dark flex-column flex-md-row bg-dark shadow-lg
+<header class="navbar navbar-dark flex-column flex-md-row bg-dark shadow-lg
         opacity-75 border-secondary-subtle border-bottom border-0 border-opacity-10">
-        <a class="navbar-brand ms-md-0 ms-md-5" href="/"><strong>.DEV Blog</strong></a>
-        <div class="navbar-nav-scroll navbar-expand">
-            <ul class="navbar-nav flex-md-row">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Postagens</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="/">Ranking</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Editores</a>
-                </li>
-            </ul>
-        </div>
-        <div class="float-end my-lg-auto text-light">
-            <a class="btn btn-outline-success ms-md-0 ms-md-2 disabled" href="/">
-                <strong>Entrar</strong>
-            </a>
-            <a class="btn btn-outline-secondary ms-md-0 ms-md-2" href="/">
-                <strong>Cadastrar</strong>
-            </a>
-        </div>
-    </header>
-    <div class="container">
-        <div class="d-flex mt-5 justify-content-center h-100">
-            <div id="login-box" class="row card m-md-5 align-self-center border-secondary w-50 shadow-lg">
-                <div class="card-header text-body-emphasis">
-                    <h5>Minha conta</h5>
-                </div>
-                <div class="card-body">
-                    <form action="php/MVCRouter" method="post">
-                        <input type="hidden" name="controller" value="login">
-                        <div class="form-group row mt-1">
-                            <label for="username" class="col-sm-3 col-form-label">Usuário</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" id="username">
-                            </div>
+    <a class="navbar-brand ms-md-0 ms-md-5" href="/"><strong>.DEV Blog</strong></a>
+    <div class="navbar-nav-scroll navbar-expand">
+        <ul class="navbar-nav flex-md-row">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Postagens</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/">Ranking</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Editores</a>
+            </li>
+        </ul>
+    </div>
+    <div class="float-end my-lg-auto text-light">
+        <a class="btn btn-outline-success ms-md-0 ms-md-2 disabled" href="/">
+            <strong>Entrar</strong>
+        </a>
+        <a class="btn btn-outline-secondary ms-md-0 ms-md-2" href="/">
+            <strong>Cadastrar</strong>
+        </a>
+    </div>
+</header>
+<div class="container">
+    <div class="d-flex mt-5 justify-content-center h-100">
+        <div id="login-box" class="row card m-md-5 align-self-center border-secondary w-50 shadow-lg">
+            <div class="card-header text-body-emphasis">
+                <h5>Minha conta</h5>
+            </div>
+            <div class="card-body">
+                <form action="php/MVCRouter" method="post">
+                    <input type="hidden" name="controller" value="login">
+                    <div class="form-group row mt-1">
+                        <label for="username" class="col-sm-3 col-form-label">Usuário</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="username">
                         </div>
-                        <div class="form-group row mt-1">
-                            <label for="password" class="col-sm-3 col-form-label">Senha</label>
-                            <div class="col-sm-8">
-                                <input type="password" class="form-control" id="password">
-                            </div>
+                    </div>
+                    <div class="form-group row mt-1">
+                        <label for="password" class="col-sm-3 col-form-label">Senha</label>
+                        <div class="col-sm-8">
+                            <input type="password" class="form-control" id="password">
                         </div>
-                        <div class="form-group col mt-3">
-                            <div class="d-flex justify-content-center">
-                                <button type="submit" class="btn btn-sm btn-primary">Entrar</button>
-                                &nbsp;&nbsp;
-                                <a href="/" class="btn btn-sm btn-outline-secondary" role="button">
-                                    <span class="glyphicon glyphicon-share-alt"></span>
-                                    Voltar
-                                </a>
-                            </div>
+                    </div>
+                    <div class="form-group col mt-3">
+                        <div class="d-flex justify-content-center">
+                            <button type="submit" class="btn btn-sm btn-primary">Entrar</button>
+                            &nbsp;&nbsp;
+                            <a href="/" class="btn btn-sm btn-outline-secondary" role="button">
+                                <span class="glyphicon glyphicon-share-alt"></span>
+                                Voltar
+                            </a>
                         </div>
-                        <?php
+                    </div>
+                    <?php
 
-                        include "php/controller/LoginController.php";
+                    require("php\PhpUtils.php");
+                    require("php\controller\LoginController.php");
 
-                        use php\controller\LoginController;
-                        use php\PhpUtils as phputils;
+                    use php\controller\LoginController as login;
+                    use php\PhpUtils as utils;
 
-                        session_start();
+                    if (login::getSingleton()->isUserSignedUp()) {
+                        utils::getSingleton()->onRedirectOk(null, "/");
+                        return;
+                    }
 
-                        $redirect = "/login";
-                        $login = LoginController::getSingleton();
-                        if ($login->isUserSignedUp()) {
-                            header("Location:/account");
+                    var_dump($_POST);
+
+                    if (isset($_POST["err"])) {
+                        $err = urldecode($_POST["err"]);
+                        if (utils::getSingleton()->checkPhpInjection($err)) {
+                            utils::getSingleton()->onRedirectErr("Php Injection detectado!", "/");
                             return;
                         }
 
-                        if (isset($_GET["err"])) {
-                            include "php/PhpUtils.php";
-
-                            $utils = phputils::getSingleton();
-                            $err = urldecode($_GET["err"]);
-                            if ($utils->checkPhpInjection($err)) {
-                                $utils->onRawIndexErr("Php Injection detectado!", $redirect);
-                                return;
-                            }
-
-                            echo "
+                        echo "
                         <div class='form-group col mt-4'>
                             <div class='d-flex justify-content-center small'>
-                                <div class='alert small alert-warning border-danger alert-dismissible fade show col-sm-12'
+                                <div class='alert small alert-warning border-warning alert-dismissible fade show col-sm-12'
                                      style='text-justify: inter-word; text-align: justify' role='alert'>
                                     <p class='mb-0'>$err</p>
-                                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'></button>
+                                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'>
+                                    </button>
                                     <hr/>
                                     <p class='mb-0'>
                                         <span class='glyphicon glyphicon-question-sign'></span>
-                                        Não possui cadastro? <a class='alert-link' href='/'>Clique aqui!</a>
+                                        Não possui cadastro? <a class='alert-link' href='/register'>Clique aqui!</a>
                                     </p>
                                 </div>
                             </div>
                             <div class='small d-flex justify-content-center'>
                             </div>
                         </div>";
-                        }
-                        ?>
-                    </form>
-                </div>
+                    }
+                    ?>
+                </form>
             </div>
         </div>
     </div>
-    <footer class="card-footer py-3 mt-auto bg-body-secondary fixed-bottom border-success-subtle border-top border-5">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-lg-5 mb-0">
-                    <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/"
-                       aria-label=".DEV Blog">
-                        <span class="fs-5">.DEV Blog</span>
-                    </a>
-                    <ul class="list-unstyled small text-muted">
-                        <li class="mb-2">
-                            <strong>Projeto Integrado III</strong>
-                        </li>
-                        <li class="mb-2">
-                            <strong>Aluno:</strong> Nádio Dib Fernandes Pontes [matrícula: <code>201918579</code>]
-                        </li>
-                        <li class="mb-2">
-                            Código licenciado pelo
-                            <a class="alert-link"
-                               href="https://github.com/Devwarlt/dotdev-blog#MIT-1-ov-file">MIT</a>.
-                        </li>
-                    </ul>
-                </div>
+</div>
+<footer class="card-footer py-3 mt-auto bg-body-secondary fixed-bottom border-success-subtle border-top border-5">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-lg-5 mb-0">
+                <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/"
+                   aria-label=".DEV Blog">
+                    <span class="fs-5">.DEV Blog</span>
+                </a>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2">
+                        <strong>Projeto Integrado III</strong>
+                    </li>
+                    <li class="mb-2">
+                        <strong>Aluno:</strong> Nádio Dib Fernandes Pontes [matrícula: <code>201918579</code>]
+                    </li>
+                    <li class="mb-2">
+                        Código licenciado pelo
+                        <a class="alert-link"
+                           href="https://github.com/Devwarlt/dotdev-blog#MIT-1-ov-file">MIT</a>.
+                    </li>
+                </ul>
             </div>
         </div>
-    </footer>
-    <script type="text/javascript" src="js/jquery-3.7.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-    <script>
-        $(function () {
-            $('#login-box').hide().fadeIn('slow');
-        });
-    </script>
+    </div>
+</footer>
+<script type="text/javascript" src="js/jquery-3.7.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script>
+    $(function () {
+        $('#login-box').hide().fadeIn('slow');
+    });
+</script>
 </body>
 </html>
