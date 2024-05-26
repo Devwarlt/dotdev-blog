ffe5258992ddf6c2e1267264615bcb256d5c3bd7
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-25 23:50:52 -0300

    WIP Implementation of HTTP_Request2 dependency

--- src/php/MVCRouter.php
+++ src/php/MVCRouter.php
@@ -1,45 +1,39 @@
 <?php
 
-include "PhpUtils.php";
+require("..\php\PhpUtils.php");
 
-use php\controller\LoginController;
-use php\PhpUtils;
-
-$utils = PhpUtils::getSingleton();
-$errRef = $okRef = "../";
+use php\controller\LoginController as login;
+use php\PhpUtils as utils;
 
 if (count($_POST) === 0 || !isset($_POST["controller"])) {
-    $utils->onRawIndexErr("Requisição inválida!", $errRef);
+    utils::getSingleton()->onRedirectErr("Requisição inválida!", "../");
     return;
 }
 
 $controller = $_POST["controller"];
 switch ($controller) {
     default:
-        $utils->onRawIndexErr("Controlador não encontrado: <strong>$controller</strong>", $errRef);
+        {
+            utils::getSingleton()->onRedirectErr(
+                "Controlador não encontrado: <strong>$controller</strong>",
+                "../"
+            );
+        }
+        break;
+    case "logout":
+        {
+            require("..\php\controller\LoginController.php");
+
+            if (login::getSingleton()->closeSession())
+                utils::getSingleton()->onRedirectOk("Logout efetuado com êxito!", "../");
+            else
+                utils::getSingleton()->onRedirectErr("Não foi possível efetuar o logout!", "../");
+        }
         break;
     case "login":
         {
-            include "controller/LoginController.php";
-            include "dao/engine/SQLQuery.php";
-            include "dao/engine/MySQLDatabase.php";
-            include "dao/LoginDAO.php";
-            include "model/LoginModel.php";
-            include "model/LoginResultModel.php";
-
-            $login = LoginController::getSingleton();
-
-            if (isset($_POST["logout"])) {
-                $result = $login->closeSession();
-                $utils->onRawIndexOk(
-                    $result ? "Logout efetuado com sucesso!" : "Não foi possível efetuar o logout!",
-                    $result ? $okRef : $errRef
-                );
-                return;
-            }
-
             if (!isset($_POST["username"]) || !isset($_POST["password"])) {
-                $utils->onRawIndexErr(
+                utils::getSingleton()->onRedirectErr(
                     "Não foi possível efetuar seu login no sistema! Realize o login com suas credenciais "
                     . "adequadamente.",
                     "../login"
@@ -47,24 +41,18 @@ switch ($controller) {
                 return;
             }
 
-            $okRef = "../account";
-            if ($login->isUserSignedUp()) {
-                header("Location:$okRef");
+            require("..\php\controller\LoginController.php");
+
+            if (login::getSingleton()->isUserSignedUp()) {
+                utils::getSingleton()->onRedirectOk(null, "../");
                 return;
             }
 
-            $loginResponse = $login->logIn($_POST["username"], $_POST["password"]);
-            if ($loginResponse->getErr() === null) {
-                if ($loginResponse->getLogin() !== null)
-                    $login->beginSession($loginResponse->getLogin());
-                header("Location:$okRef");
+            if (($response = login::getSingleton()->logIn($_POST["username"], $_POST["password"]))->getErr() === null) {
+                login::getSingleton()->beginSession($response->getLogin());
+                utils::getSingleton()->onRedirectOk(null, "../");
             } else
-                $utils->onRawIndexErr($loginResponse->getErr(), $errRef);
+                utils::getSingleton()->onRedirectErr($response->getErr(), "../");
         }
         break;
-}
-
-function pJustified(string $msg): string
-{
-    return "<p style='text-align: justify; text-justify: inter-word'>$msg</p>";
 }
--- src/php/PhpUtils.php
+++ src/php/PhpUtils.php
@@ -2,39 +2,101 @@
 
 namespace php;
 
+use HTTP_Request2;
+use HTTP_Request2_Exception;
+
 final class PhpUtils
 {
     public static $singleton;
     private static string $php_injection_regex_pattern = '/^(?=.*<\?)|(?=.*\?>).*$/';
 
-    private function __construct() { }
+    private function __construct()
+    {
+    }
 
     public static function getSingleton(): PhpUtils
     {
-        if (self::$singleton === null) self::$singleton = new PhpUtils();
+        if (self::$singleton === null)
+            self::$singleton = new PhpUtils();
         return self::$singleton;
     }
 
-    public function onRawIndexErr(string $msg, string $ref): void { self::onRawRedirect($msg, $ref, "err"); }
-
-    private function onRawRedirect(string $msg, string $ref, string $var): void
+    public function onRedirectErr(string $msg, string $ref): void
     {
-        header("Location:$ref?$var=" . urlencode($msg));
+        self::onHeaderRedirect($ref, ["err" => $msg], HTTP_Request2::METHOD_POST);
     }
 
-    public function onRawIndexOk(string $msg, string $ref): void { self::onRawRedirect($msg, $ref, "ok"); }
+    /**
+     * # RFC 2068 - Hypertext Transfer Protocol -- HTTP/1.1
+     * <p align="justify">
+     *     This document specifies an Internet standards track protocol for the
+     * Internet community, and requests discussion and suggestions for
+     * improvements.  Please refer to the current edition of the "Internet
+     * Official Protocol Standards" (STD 1) for the standardization state
+     * and status of this protocol.
+     * Distribution of this memo is unlimited.
+     * </p>
+     *
+     * <br/>
+     *
+     * ## References:
+     * 1. `What does enctype='multipart/form-data' mean?`
+     * 1. `form-data vs -urlencoded`
+     * @version 2068
+     * @since 1990
+     * @link http://www.faqs.org/rfcs/rfc2616.html
+     * @link http://www.faqs.org/rfcs/rfc2068.html
+     * @see https://www.php.net/manual/en/function.header
+     * @see https://stackoverflow.com/a/4526286
+     * @see https://gist.github.com/joyrexus/524c7e811e4abf9afe56
+     * @author R. Fielding
+     * @author UC Irvine
+     * @author J. Gettys
+     * @author J. Mogul
+     * @author DEC
+     * @author H. Frystyk
+     * @author T. Berners-Lee
+     * @copyright The Internet Society (1999) / MIT / LCS
+     **/
+    private function onHeaderRedirect(string $ref, array $args, string $method): void
+    {
+        switch ($method) {
+            case HTTP_Request2::METHOD_GET:
+            default:
+                {
+                    $formatted_args = [];
+                    foreach ($args as $k => $v)
+                        $formatted_args[] = $k . "=" . urlencode($v);
+
+                    header("Location" . $ref . "?" . implode('&', $formatted_args));
+                }
+                break;
+            case HTTP_Request2::METHOD_POST:
+                {
+                    $request = new HTTP_Request2($ref, $method);
+                    foreach ($args as $k => $v)
+                        $request->addPostParameter($k, $v);
+
+                    try {
+                        $request->send();
+                    } catch (HTTP_Request2_Exception $err) {
+                        self::onHeaderRedirect(
+                            "../",
+                            ["err" => $err->getMessage()],
+                            HTTP_Request2::METHOD_GET);
+                    }
+                }
+                break;
+        }
+    }
+
+    public function onRedirectOk(?string $msg, string $ref): void
+    {
+        self::onHeaderRedirect($ref, ["ok" => $msg], HTTP_Request2::METHOD_POST);
+    }
 
     public function checkPhpInjection(string $str): bool
     {
         return preg_match(self::$php_injection_regex_pattern, $str);
-    }
-
-    public function getContents(string $path): string
-    {
-        $file = dirname(__FILE__) . $path;
-        if (!file_exists($file))
-            return "<p style='color: red'><strong>Arquivo não encontrado:</strong> "
-                . dirname(__FILE__) . "$path</p>";
-        return file_get_contents($file);
     }
 }
