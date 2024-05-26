a09a896076aa7cbf3e8516f2894c99d5d9291901
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-25 23:49:28 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,32 +4,35 @@ CREATE DATABASE IF NOT EXISTS `dotdev-db` DEFAULT CHARACTER SET latin1 COLLATE l
 
 USE `dotdev-db`;
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `username` TINYTEXT NOT NULL,
-    `password` VARCHAR(64) NOT NULL,
-    `email` VARCHAR(320) NOT NULL,
-    `level` TINYINT NOT NULL DEFAULT 0, -- 0: default / 1: mod / 2: admin
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `username` TINYTEXT     NOT NULL,
+    `password` VARCHAR(64)  NOT NULL,
+    `email`    VARCHAR(320) NOT NULL,
+    `level`    TINYINT      NOT NULL DEFAULT 0, -- 0: default / 1: mod / 2: admin
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `posts` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `title` TEXT(512) NOT NULL,
-    `text` LONGTEXT NOT NULL,
-    `owner_id` INT NOT NULL,
-    `views` INT NOT NULL DEFAULT 0,
-    `total_votes` INT NOT NULL DEFAULT 0,
-    `average_score` FLOAT(2, 2) NOT NULL DEFAULT 0.00,
-    `creation_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `last_updated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `last_update_user_id` INT NOT NULL DEFAULT -1,
-    `hidden` BOOLEAN NOT NULL DEFAULT 0,
+CREATE TABLE IF NOT EXISTS `posts`
+(
+    `id`                  INT         NOT NULL AUTO_INCREMENT,
+    `title`               TEXT(512)   NOT NULL,
+    `text`                LONGTEXT    NOT NULL,
+    `owner_id`            INT         NOT NULL,
+    `views`               INT         NOT NULL DEFAULT 0,
+    `total_votes`         INT         NOT NULL DEFAULT 0,
+    `average_score`       FLOAT(2, 2) NOT NULL DEFAULT 0.00,
+    `creation_date`       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_updated`        DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_update_user_id` INT         NOT NULL DEFAULT -1,
+    `hidden`              BOOLEAN     NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-ALTER TABLE `posts` ADD CONSTRAINT `posts_owner_id_users_id` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
-    ON DELETE CASCADE;
+ALTER TABLE `posts`
+    ADD CONSTRAINT `posts_owner_id_users_id` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
+        ON DELETE CASCADE;
 
 INSERT INTO `users` (`username`, `password`, `email`, `level`)
-    VALUES ('root', SHA2('toor', 256), 'nadio.engsoft@gmail.com', 2);
+VALUES ('root', SHA2('toor', 256), 'nadio.engsoft@gmail.com', 2);
