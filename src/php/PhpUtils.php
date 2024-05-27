14642079b4ee7672dd6ec2218bcb25087afaca46
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-26 21:38:38 -0300

    Update PhpUtils.php

--- src/php/PhpUtils.php
+++ src/php/PhpUtils.php
@@ -2,8 +2,9 @@
 
 namespace php;
 
-use HTTP_Request2;
-use HTTP_Request2_Exception;
+define("RESPONSE_SUCCESS", 'ok_response');
+define("RESPONSE_FAILURE", 'err_response');
+define("RESPONSE_COOLDOWN", "1 days");
 
 final class PhpUtils
 {
@@ -23,80 +24,47 @@ final class PhpUtils
 
     public function onRedirectErr(string $msg, string $ref): void
     {
-        self::onHeaderRedirect($ref, ["err" => $msg], HTTP_Request2::METHOD_POST);
+        self::setResponseCookie(RESPONSE_FAILURE, $msg);
+        header("Location:$ref");
     }
 
-    /**
-     * # RFC 2068 - Hypertext Transfer Protocol -- HTTP/1.1
-     * <p align="justify">
-     *     This document specifies an Internet standards track protocol for the
-     * Internet community, and requests discussion and suggestions for
-     * improvements.  Please refer to the current edition of the "Internet
-     * Official Protocol Standards" (STD 1) for the standardization state
-     * and status of this protocol.
-     * Distribution of this memo is unlimited.
-     * </p>
-     *
-     * <br/>
-     *
-     * ## References:
-     * 1. `What does enctype='multipart/form-data' mean?`
-     * 1. `form-data vs -urlencoded`
-     * @version 2068
-     * @since 1990
-     * @link http://www.faqs.org/rfcs/rfc2616.html
-     * @link http://www.faqs.org/rfcs/rfc2068.html
-     * @see https://www.php.net/manual/en/function.header
-     * @see https://stackoverflow.com/a/4526286
-     * @see https://gist.github.com/joyrexus/524c7e811e4abf9afe56
-     * @author R. Fielding
-     * @author UC Irvine
-     * @author J. Gettys
-     * @author J. Mogul
-     * @author DEC
-     * @author H. Frystyk
-     * @author T. Berners-Lee
-     * @copyright The Internet Society (1999) / MIT / LCS
-     **/
-    private function onHeaderRedirect(string $ref, array $args, string $method): void
+    private function setResponseCookie(string $name, ?string $msg): void
     {
-        switch ($method) {
-            case HTTP_Request2::METHOD_GET:
-            default:
-                {
-                    $formatted_args = [];
-                    foreach ($args as $k => $v)
-                        $formatted_args[] = $k . "=" . urlencode($v);
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
 
-                    header("Location" . $ref . "?" . implode('&', $formatted_args));
-                }
-                break;
-            case HTTP_Request2::METHOD_POST:
-                {
-                    $request = new HTTP_Request2($ref, $method);
-                    foreach ($args as $k => $v)
-                        $request->addPostParameter($k, $v);
-
-                    try {
-                        $request->send();
-                    } catch (HTTP_Request2_Exception $err) {
-                        self::onHeaderRedirect(
-                            "../",
-                            ["err" => $err->getMessage()],
-                            HTTP_Request2::METHOD_GET);
-                    }
-                }
-                break;
-        }
+        setcookie($name, $msg, strtotime("+" . RESPONSE_COOLDOWN), "/");
     }
 
     public function onRedirectOk(?string $msg, string $ref): void
     {
-        self::onHeaderRedirect($ref, ["ok" => $msg], HTTP_Request2::METHOD_POST);
+        self::setResponseCookie(RESPONSE_SUCCESS, $msg);
+        header("Location:$ref");
     }
 
     public function checkPhpInjection(string $str): bool
     {
         return preg_match(self::$php_injection_regex_pattern, $str);
+    }
+
+    public function getResponseCookie(string $name, bool $unset): ?string
+    {
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
+        $value = $_COOKIE[$name] ?? null;
+
+        if ($unset)
+            self::unsetResponseCookie($name);
+
+        return $value;
+    }
+
+    public function unsetResponseCookie(string $name): void
+    {
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
+        setcookie($name, "", strtotime("-" . RESPONSE_COOLDOWN), "/");
     }
 }
