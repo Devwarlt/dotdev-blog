87986eda4d32e2774518820e4c0034ec53d0169b
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-26 21:38:28 -0300

    Update LoginResultModel.php

--- src/php/model/LoginResultModel.php
+++ src/php/model/LoginResultModel.php
@@ -8,17 +8,37 @@ final class LoginResultModel
     private string $err;
     private bool $status = false;
 
-    public function __construct() { }
+    public function __construct()
+    {
+    }
 
-    public function getLogin(): ?LoginModel { return $this->login; }
+    public function getLogin(): ?LoginModel
+    {
+        return $this->login;
+    }
 
-    public function getErr(): string { return $this->err; }
+    public function setLogin(?LoginModel $login): void
+    {
+        $this->login = $login;
+    }
 
-    public function getStatus() : bool { return $this->status; }
+    public function getErr(): string
+    {
+        return $this->err;
+    }
 
-    public function setLogin(?LoginModel $login) : void { $this->login = $login; }
+    public function setErr(string $err): void
+    {
+        $this->err = $err;
+    }
 
-    public function setErr(string $err) : void { $this->err = $err; }
+    public function getStatus(): bool
+    {
+        return $this->status;
+    }
 
-    public function setStatus(bool $status) : void { $this->status = $status; }
+    public function setStatus(bool $status): void
+    {
+        $this->status = $status;
+    }
 }
