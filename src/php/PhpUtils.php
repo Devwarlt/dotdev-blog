ca64be91d099e017c7871dcd947022bb7a635dee
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-28 23:30:28 -0300

    Update PhpUtils.php

--- src/php/PhpUtils.php
+++ src/php/PhpUtils.php
@@ -6,6 +6,10 @@ define("RESPONSE_SUCCESS", 'ok_response');
 define("RESPONSE_FAILURE", 'err_response');
 define("RESPONSE_COOLDOWN", "1 days");
 
+define("CREDENTIALS_NOT_SET_LOGIN", 0);
+define("CREDENTIALS_NOT_SET_REGISTER", 1);
+define("CREDENTIALS_PHP_INJECTION", 2);
+
 final class PhpUtils
 {
     public static $singleton;
@@ -22,36 +26,19 @@ final class PhpUtils
         return self::$singleton;
     }
 
-    public function onRedirectErr(string $msg, string $ref): void
-    {
-        self::setResponseCookie(RESPONSE_FAILURE, $msg);
-        header("Location:$ref");
-    }
-
-    private function setResponseCookie(string $name, ?string $msg): void
-    {
-        if (session_status() === PHP_SESSION_NONE)
-            session_start();
-
-        setcookie($name, $msg, strtotime("+" . RESPONSE_COOLDOWN), "/");
-    }
-
     public function onRedirectOk(?string $msg, string $ref): void
     {
         self::setResponseCookie(RESPONSE_SUCCESS, $msg);
         header("Location:$ref");
     }
 
-    public function checkPhpInjection(string $str): bool
+    private function setResponseCookie(string $name, ?string $msg): void
     {
-        return preg_match(self::$php_injection_regex_pattern, $str);
+        setcookie($name, $msg, strtotime("+" . RESPONSE_COOLDOWN), "/");
     }
 
     public function getResponseCookie(string $name, bool $unset): ?string
     {
-        if (session_status() === PHP_SESSION_NONE)
-            session_start();
-
         $value = $_COOKIE[$name] ?? null;
 
         if ($unset)
@@ -62,9 +49,70 @@ final class PhpUtils
 
     public function unsetResponseCookie(string $name): void
     {
-        if (session_status() === PHP_SESSION_NONE)
-            session_start();
+        if (isset($_COOKIE[$name])) {
+            unset($_COOKIE[$name]);
+            setcookie($name, "", -1, "/");
+        }
+    }
 
-        setcookie($name, "", strtotime("-" . RESPONSE_COOLDOWN), "/");
+    public function validateCredentials(?string $username, ?string $password, string $controller): int
+    {
+        if (!isset($username) || !isset($password))
+            return $controller === "login" ? CREDENTIALS_NOT_SET_LOGIN : CREDENTIALS_NOT_SET_REGISTER;
+
+        if (self::checkPhpInjection($username) || self::checkPhpInjection($password))
+            return CREDENTIALS_PHP_INJECTION;
+    }
+
+    public function checkPhpInjection(string... $params): bool
+    {
+        $matches = function (string... $args) {
+            $result = [];
+            foreach ($args as $arg)
+                $result[] = preg_match(self::$php_injection_regex_pattern, $arg);
+            return $result;
+        };
+        return in_array(true, $matches($params));
+    }
+
+    public function isErrRedirect(int $credentialsFlag, string $ref): bool
+    {
+        switch ($credentialsFlag) {
+            default:
+            {
+                return false;
+            }
+            case CREDENTIALS_NOT_SET_REGISTER:
+            {
+                self::onRedirectErr(
+                    "Não foi possível efetuar seu registro no sistema! Realize o seu cadastro novamente.",
+                    $ref
+                );
+                return true;
+            }
+            case CREDENTIALS_NOT_SET_LOGIN:
+            {
+                self::onRedirectErr(
+                    "Não foi possível efetuar seu login no sistema! Realize o login com suas credenciais "
+                    . "adequadamente.",
+                    $ref
+                );
+                return true;
+            }
+            case CREDENTIALS_PHP_INJECTION:
+            {
+                self::onRedirectErr(
+                    "Php Inject detectado em um dos parâmetros enviados ao servidor!",
+                    $ref
+                );
+                return true;
+            }
+        }
+    }
+
+    public function onRedirectErr(string $msg, string $ref): void
+    {
+        self::setResponseCookie(RESPONSE_FAILURE, $msg);
+        header("Location:$ref");
     }
 }
