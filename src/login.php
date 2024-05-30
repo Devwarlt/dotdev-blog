7552cdc72d155494ff2d52228b8bad967c100201
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-29 21:10:48 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -1,3 +1,22 @@
+<?php
+
+require('php\PhpUtils.php');
+require('php\controller\LoginController.php');
+
+use php\controller\LoginController as login;
+use php\PhpUtils as utils;
+
+if (login::getSingleton()->isUserSignedUp()) {
+    utils::getSingleton()->onRedirectOk(
+        "Você já está entrou no sistema, não é necessário realizar outro login.",
+        "/"
+    );
+    return;
+}
+
+if ($_SERVER["HTTP_REFERER"] !== $_SERVER["REQUEST_URI"])
+    utils::getSingleton()->flushResponseCookies();
+?>
 <!DOCTYPE html>
 <html lang="pt" style="height: 100%;">
 <head>
@@ -10,8 +29,12 @@
 <body class="bg-image">
 <header class="navbar navbar-dark flex-column flex-md-row bg-dark shadow-lg
         opacity-75 border-secondary-subtle border-bottom border-0 border-opacity-10">
-    <a class="navbar-brand ms-md-0 ms-md-5" href="/"><strong>.DEV Blog</strong></a>
-    <div class="navbar-nav-scroll navbar-expand">
+    <a class="navbar-brand ms-md-0 ms-md-5 shadow-sm link-light" href="/">
+        <span class="glyphicon glyphicon-console bg-black rounded-top-1"
+              style="padding: 4px 8px 4px 8px; border-top: 6px solid goldenrod"></span>&nbsp;
+        <strong>.DEV Blog</strong>
+    </a>
+    <div class="navbar-nav-scroll navbar-expand navbar-toggler">
         <ul class="navbar-nav flex-md-row">
             <li class="nav-item">
                 <a class="nav-link" href="/posts">Postagens</a>
@@ -24,12 +47,12 @@
             </li>
         </ul>
     </div>
-    <div class="float-end my-lg-auto text-light">
-        <a class="btn btn-outline-success ms-md-0 ms-md-2 disabled" href="/login">
-            <strong>Entrar</strong>
+    <div class="float-end my-lg-auto text-light" style="padding-right: 4px">
+        <a class="btn btn-sm btn-outline-success ms-md-0 ms-md-2 active disabled" href="/login">
+            <strong>Entrar</strong> <span class="glyphicon glyphicon-log-in"></span>
         </a>
-        <a class="btn btn-outline-secondary ms-md-0 ms-md-2" href="/new_account">
-            <strong>Nova conta</strong>
+        <a class="btn btn-sm btn-outline-secondary ms-md-0 ms-md-2" href="/register">
+            <strong>Registrar</strong> <span class="glyphicon glyphicon-info-sign"></span>
         </a>
     </div>
 </header>
@@ -45,13 +68,13 @@
                     <div class="form-group row mt-1">
                         <label for="username" class="col-sm-3 col-form-label">Usuário</label>
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="username">
+                            <input type="text" class="form-control" id="username" name="username">
                         </div>
                     </div>
                     <div class="form-group row mt-1">
                         <label for="password" class="col-sm-3 col-form-label">Senha</label>
                         <div class="col-sm-8">
-                            <input type="password" class="form-control" id="password">
+                            <input type="password" class="form-control" id="password" name="password">
                         </div>
                     </div>
                     <div class="form-group col mt-3">
@@ -64,48 +87,34 @@
                             </a>
                         </div>
                     </div>
-                    <?php
-
-                    require("php\controller\LoginController.php");
-                    require("php\PhpUtils.php");
-
-                    use php\controller\LoginController as login;
-                    use php\PhpUtils as utils;
-
-                    if (login::getSingleton()->isUserSignedUp()) {
-                        utils::getSingleton()->onRedirectOk(null, "/");
-                        return;
-                    }
-
-                    if (!is_null($err = utils::getSingleton()->getResponseCookie(RESPONSE_FAILURE, true)))
-                        echo "
-                    <div class='form-group col mt-4'>
-                        <div class='d-flex justify-content-center small'>
-                            <div class='alert small alert-warning border-warning alert-dismissible fade show col-sm-12'
-                                 style='text-justify: inter-word; text-align: justify' role='alert'>
-                                <p class='mb-0'>" . $err . "</p>
-                                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Fechar'>
-                                </button>
-                                <hr/>
-                                <p class='mb-0'>
-                                    <span class='glyphicon glyphicon-question-sign'></span>
-                                    Não possui cadastro? <a class='alert-link' href='/new_account'>Clique aqui!</a>
-                                </p>
+                    <?php if (!is_null($err = utils::getSingleton()->getResponseCookie(RESPONSE_FAILURE, true))) { ?>
+                        <div class="form-group col mt-3">
+                            <div class="d-flex justify-content-center small">
+                                <div class="alert small alert-info border-info alert-dismissible
+                                    fade show col-sm-12 shadow"
+                                     style="text-justify: inter-word; text-align: justify" role="alert">
+                                    <p class="mb-0">
+                                        <span class="glyphicon glyphicon-info-sign"></span> <?php echo $err; ?>
+                                    </p>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fechar">
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>";
-                    ?>
+                    <?php } ?>
                 </form>
             </div>
         </div>
     </div>
 </div>
-<footer class="card-footer py-3 mt-auto bg-body-secondary fixed-bottom border-success-subtle border-top border-5">
+<footer class="card-footer py-3 mt-auto bg-body-secondary small fixed-bottom border-success-subtle border-top border-5">
     <div class="container py-4">
         <div class="row">
             <div class="col-lg-5 mb-0">
                 <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/"
                    aria-label=".DEV Blog">
+                    <span class="glyphicon glyphicon-console text-light bg-secondary rounded-top-1"
+                          style="padding: 4px 8px 4px 8px; border-top: 6px solid darkslategray"></span>&nbsp;
                     <span class="fs-5">.DEV Blog</span>
                 </a>
                 <ul class="list-unstyled small text-muted">
