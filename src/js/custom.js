a65ab686f93b153ea92581bf01dfd629651eb083
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-31 19:18:59 -0300

    Update custom.js

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,43 +1,29 @@
-function passwordContainsLowercaseLetter(value) {
+const passwordContainsLowercaseLetter = (value) => {
     return /[a-z]/.test(value);
-}
-
-function passwordContainsUppercaseLetter(value) {
+}, passwordContainsUppercaseLetter = (value) => {
     return /[A-Z]/.test(value);
-}
-
-function passwordContainsSpaces(value) {
+}, passwordContainsSpaces = (value) => {
     return / /.test(value);
-}
-
-function passwordContainsNumber(value) {
+}, passwordContainsNumber = (value) => {
     return /[0-9]/.test(value);
-}
-
-function passwordContainsSymbol(value) {
-    let containsSymbol = false,
-        symbols = "-!§$%&/()=?.:,~;'#+-/*\\|{}[]_<>\"".split("");
-
+}, passwordContainsSymbol = (value) => {
+    let containsSymbol = false, symbols = "-!§$%&/()=?.:,~;'#+-/*\\|{}[]_<>\"".split("");
     $.each(symbols, function (index, symbol) {
         if (value.indexOf(symbol) > -1) {
             containsSymbol = true;
-
-            // We found a symbol. Therefore, return false to exit $.each early (short-circuit).
             return false;
         }
     });
-
     return containsSymbol;
-}
-
-function countSpaces(value) {
+}, countSpaces = (value) => {
     return value.split(/ +/).length - 1;
-}
-
-let password = document.getElementById("password");
-let power = document.getElementById("power-point");
-password.oninput = function () {
-    const options = {
+}, toggleAll = (masterBox) => {
+    let checkBoxes = document.getElementsByTagName('input');
+    for (let i = 0; i < checkBoxes.length; i++)
+        if (checkBoxes[i].className.includes('checkbox-child'))
+            checkBoxes[i].checked = masterBox.checked;
+}, passwordInputHandler = (password) => {
+    const power = document.getElementById("power-point"), options = {
         cssCustom: "progress-bar bg-secondary rounded-2",
         cssExtra: "bg-gradient",
         maxScore: 30,
@@ -75,4 +61,6 @@ password.oninput = function () {
         ? options.colors.length - 1 : adjustedIndexOffset;
     power.className = `${options.cssCustom} ${options.colors[adjustedIndexOffset][0]} ${options.cssExtra}`;
     power.innerHTML = options.colors[adjustedIndexOffset][1];
+}, fetchCookieByName = (name) => {
+    return document.cookie.split("; ").find((row) => row.startsWith(`${name}=`))?.split("=")[1];
 };
