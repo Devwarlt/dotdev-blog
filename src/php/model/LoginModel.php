28e6bf2b95df1eabbc54349c63f0e090029fa744
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:49 -0300

    Update LoginModel.php

--- src/php/model/LoginModel.php
+++ src/php/model/LoginModel.php
@@ -1,61 +1,57 @@
 <?php
+	namespace php\model
+	{
+		final class LoginModel
+		{
+			private int $id;
+			private string $username;
+			private string $password;
+			private int $level;
 
-namespace php\model;
+			public function __construct(
+				int $id,
+				string $username,
+				string $password,
+				int $level
+			) {
+				$this->id = $id;
+				$this->username = $username;
+				$this->password = $password;
+				$this->level = $level;
+			}
 
-final class LoginModel
-{
-    private int $id;
-    private string $username;
-    private string $password;
-    private int $level;
+			public function getId() : int {
+				return $this->id;
+			}
 
-    public function __construct(int $id,
-        string $username,
-        string $password,
-        int $level)
-    {
-        $this->id = $id;
-        $this->username = $username;
-        $this->password = $password;
-        $this->level = $level;
-    }
+			public function getUsername() : string {
+				return $this->username;
+			}
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
+			public function getPassword() : string {
+				return $this->password;
+			}
 
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
+			public function getLevel() : int {
+				return $this->level;
+			}
 
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function getLevel(): int
-    {
-        return $this->level;
-    }
-
-    public function getLevelHumanReadable(): string
-    {
-        switch ($this->level) {
-            case 0:
-            default:
-            {
-                return "normal";
-            }
-            case 1:
-            {
-                return "moderador";
-            }
-            case 2:
-            {
-                return "adminstrador";
-            }
-        }
-    }
-}
+			public function getLevelHumanReadable() : string {
+				switch ($this->level) {
+					case 0:
+					default:
+						{
+							return "normal";
+						}
+					case 1:
+						{
+							return "moderador";
+						}
+					case 2:
+						{
+							return "adminstrador";
+						}
+				}
+			}
+		}
+	}
