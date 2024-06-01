7957982fabb5120848ca43580ebc1890245c3e51
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:45 -0300

    Update SQLQuery.php

--- src/php/dao/engine/SQLQuery.php
+++ src/php/dao/engine/SQLQuery.php
@@ -1,31 +1,34 @@
 <?php
+	namespace php\dao\engine
+	{
+		final class SQLQuery
+		{
+			private ?array $params;
+			private string $sql;
 
-namespace php\dao\engine;
+			public function __construct(
+				string $sql,
+				array $params = null
+			) {
+				$this->sql = $sql;
+				$this->params = $params;
+			}
 
-final class SQLQuery
-{
-    private ?array $params;
-    private string $sql;
+			public function getQuery() : string {
+				if ($this->params !== null) {
+					$this->sql = self::replaceArray($this->sql, $this->params);
+				}
+				return $this->sql;
+			}
 
-    public function __construct(string $sql,
-        array $params = null)
-    {
-        $this->sql = $sql;
-        $this->params = $params;
-    }
-
-    public function getQuery(): string
-    {
-        if ($this->params !== null)
-            $this->sql = self::replaceArray($this->sql,$this->params);
-        return $this->sql;
-    }
-
-    private static function replaceArray(string $value,
-        array $array): string
-    {
-        foreach ($array as $innerKey => $innerValue)
-            $value = str_replace($innerKey,$innerValue,$value);
-        return $value;
-    }
-}
+			private static function replaceArray(
+				string $value,
+				array $array
+			) : string {
+				foreach ($array as $innerKey => $innerValue) {
+					$value = str_replace($innerKey, $innerValue, $value);
+				}
+				return $value;
+			}
+		}
+	}
