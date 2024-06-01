440b9b23a24149f00ce7bfb7f3becc4e88e2b19c
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:26 -0300

    Create PostResultModel.php

---
+++ src/php/model/PostResultModel.php
@@ -0,0 +1,36 @@
+<?php
+	namespace php\model;
+
+	final class PostResultModel
+	{
+		private ?PostModel $post;
+		private string $err = "";
+		private bool $status = false;
+
+		public function __construct() {
+		}
+
+		public function getPost() : ?PostModel {
+			return $this->post;
+		}
+
+		public function setPost(?PostModel $post) : void {
+			$this->post = $post;
+		}
+
+		public function getErr() : string {
+			return $this->err;
+		}
+
+		public function setErr(string $err) : void {
+			$this->err = $err;
+		}
+
+		public function isStatus() : bool {
+			return $this->status;
+		}
+
+		public function setStatus(bool $status) : void {
+			$this->status = $status;
+		}
+	}
