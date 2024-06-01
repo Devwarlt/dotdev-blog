8812efe4b194512a5f72440c5af009f700189b5c
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:50 -0300

    Update LoginResultModel.php

--- src/php/model/LoginResultModel.php
+++ src/php/model/LoginResultModel.php
@@ -1,44 +1,37 @@
 <?php
+	namespace php\model
+	{
+		final class LoginResultModel
+		{
+			private ?LoginModel $login;
+			private string $err = "";
+			private bool $status = false;
 
-namespace php\model;
+			public function __construct() {
+			}
 
-final class LoginResultModel
-{
-    private ?LoginModel $login;
-    private string $err = "";
-    private bool $status = false;
+			public function getLogin() : ?LoginModel {
+				return $this->login;
+			}
 
-    public function __construct()
-    {
-    }
+			public function setLogin(?LoginModel $login) : void {
+				$this->login = $login;
+			}
 
-    public function getLogin(): ?LoginModel
-    {
-        return $this->login;
-    }
+			public function getErr() : string {
+				return $this->err;
+			}
 
-    public function setLogin(?LoginModel $login): void
-    {
-        $this->login = $login;
-    }
+			public function setErr(string $err) : void {
+				$this->err = $err;
+			}
 
-    public function getErr(): string
-    {
-        return $this->err;
-    }
+			public function getStatus() : bool {
+				return $this->status;
+			}
 
-    public function setErr(string $err): void
-    {
-        $this->err = $err;
-    }
-
-    public function getStatus(): bool
-    {
-        return $this->status;
-    }
-
-    public function setStatus(bool $status): void
-    {
-        $this->status = $status;
-    }
-}
+			public function setStatus(bool $status) : void {
+				$this->status = $status;
+			}
+		}
+	}
