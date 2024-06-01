baa37897c6705c00a6a85716f3a53a622b6d47c1
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:46 -0300

    Update LoginDAO.php

--- src/php/dao/LoginDAO.php
+++ src/php/dao/LoginDAO.php
@@ -1,80 +1,71 @@
 <?php
+	namespace php\dao
+	{
 
-namespace php\dao;
+		use PDO;
+		use php\dao\engine\MySQLDatabase;
+		use php\dao\engine\SQLQuery;
+		use php\model\LoginModel;
 
-use PDO;
-use php\dao\engine\MySQLDatabase;
-use php\dao\engine\SQLQuery;
-use php\model\LoginModel;
+		final class LoginDAO
+		{
+			private static $singleton;
 
-final class LoginDAO
-{
-    private static $singleton;
+			private function __construct() { }
 
-    private function __construct()
-    {
-    }
+			public function signIn(LoginModel $login) : bool {
+				return MySQLDatabase::getSingleton()->insert(new SQLQuery("
+			INSERT INTO
+				users(username,
+				      password,
+				      level)
+                VALUES(':username',
+                       SHA2(':password', 256),
+                       :level)", [
+					":username" => $login->getUsername(),
+					":password" => $login->getPassword(),
+					":level"    => $login->getLevel()
+				]));
+			}
 
-    public function signIn(LoginModel $login): bool
-    {
-        $mysql = MySQLDatabase::getSingleton();
-        return $mysql->insert(
-            new SQLQuery(
-                "INSERT INTO `users`(`username`, `password`, `level`)
-                    VALUES (':username', SHA2(':password', 256), :level)",
-                [
-                    ":username" => $login->getUsername(),
-                    ":password" => $login->getPassword(),
-                    ":level" => $login->getLevel()
-                ]
-            )
-        );
-    }
+			public static function getSingleton() : LoginDAO {
+				if (self::$singleton === null) self::$singleton = new LoginDAO();
+				return self::$singleton;
+			}
 
-    public static function getSingleton(): LoginDAO
-    {
-        if (self::$singleton === null) self::$singleton = new LoginDAO();
-        return self::$singleton;
-    }
+			public function signUp(LoginModel $login) : ?LoginModel {
+				$result = MySQLDatabase::getSingleton()->select(new SQLQuery("
+			SELECT
+				id, username, password, level
+			FROM
+				users
+			WHERE
+				username = ':username'
+			  AND
+				password = SHA2(':password', 256)", [
+					":username" => $login->getUsername(),
+					":password" => $login->getPassword()
+				]));
+				if ($result === null) return null;
+				$data = $result->fetch(PDO::FETCH_OBJ);
+				return new LoginModel($data->id, $login->getUsername(), $login->getPassword(), $data->level);
+			}
 
-    public function signUp(LoginModel $login): ?LoginModel
-    {
-        $mysql = MySQLDatabase::getSingleton();
-        $result = $mysql->select(
-            new SQLQuery(
-                "SELECT * FROM `users` WHERE `username` = ':username' AND `password` = SHA2(':password', 256)",
-                [
-                    ":username" => $login->getUsername(),
-                    ":password" => $login->getPassword()
-                ]
-            )
-        );
-
-        if ($result === null) return null;
-
-        $data = $result->fetch(PDO::FETCH_OBJ);
-        return new LoginModel(
-            $data->id,
-            $login->getUsername(),
-            $login->getPassword(),
-            $data->level
-        );
-    }
-
-    public function updateCredentials(LoginModel $login): bool
-    {
-        $mysql = MySQLDatabase::getSingleton();
-        return $mysql->update(
-            new SQLQuery(
-                "UPDATE `users` SET `username` = ':username', `password` = SHA2(':password', 256),
-                   `level` = :level WHERE `id` = :id",
-                [
-                    ":id" => $login->getId(),
-                    ":username" => $login->getUsername(),
-                    ":password" => $login->getPassword(),
-                    ":level" => $login->getLevel()
-                ]
-            )
-        );
-    }
-}
+			public function updateCredentials(LoginModel $login) : bool {
+				return MySQLDatabase::getSingleton()->update(new SQLQuery("
+			UPDATE
+				users
+			SET
+				username = ':username',
+				password = SHA2(':password', 256),
+				level = :level
+			WHERE
+				id = :id", [
+					":id"       => $login->getId(),
+					":username" => $login->getUsername(),
+					":password" => $login->getPassword(),
+					":level"    => $login->getLevel()
+				]));
+			}
+		}
+	}
