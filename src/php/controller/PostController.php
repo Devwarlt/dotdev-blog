c1a87ad6baac3c985317e29bfd3f4daba557cde1
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:41 -0300

    Create PostController.php

---
+++ src/php/controller/PostController.php
@@ -0,0 +1,24 @@
+<?php
+	namespace php\controller
+	{
+		use php\model\PostResultModel;
+
+		final class PostController
+		{
+			private static $singleton;
+
+			private function __construct() {
+			}
+
+			public static function getSingleton() : PostController {
+				if (self::$singleton === null) self::$singleton = new PostController();
+				return self::$singleton;
+			}
+
+			public function create() : PostResultModel {
+				# TODO: this method must be implemented.
+				$result = new PostResultModel();
+				return $result;
+			}
+		}
+	}
