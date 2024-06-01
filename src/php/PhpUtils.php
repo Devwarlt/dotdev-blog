a8386ed8f7fc68707d4c8ee222fe98f2a0060d2d
Devwarlt <nadio.pontes@redes.unb.br>
2024-06-01 18:55:16 -0300

    Update PhpUtils.php

--- src/php/PhpUtils.php
+++ src/php/PhpUtils.php
@@ -1,79 +1,78 @@
 <?php
+	namespace php;
+	define("RESPONSE_SUCCESS", "ok_response");
+	define("RESPONSE_FAILURE", "err_response");
+	define("RESPONSE_COOLDOWN", "1 days");
 
-namespace php;
+	final class PhpUtils
+	{
+		public static $singleton;
+		private static string $PHP_INJECTION_REGEX_PATTERN = '/^(?=.*<\?)|(?=.*\?>).*$/';
 
-define("RESPONSE_SUCCESS","ok_response");
-define("RESPONSE_FAILURE","err_response");
-define("RESPONSE_COOLDOWN","1 days");
+		private function __construct() {
+		}
 
-final class PhpUtils
-{
-    public static $singleton;
+		public static function getSingleton() : PhpUtils {
+			if (self::$singleton === null) {
+				self::$singleton = new PhpUtils();
+			}
+			return self::$singleton;
+		}
 
-    private static string $PHP_INJECTION_REGEX_PATTERN = '/^(?=.*<\?)|(?=.*\?>).*$/';
+		public function onRedirectOk(
+			?string $msg,
+			string $ref
+		) : void {
+			self::setResponseCookie(RESPONSE_SUCCESS, $msg);
+			header("Location:$ref");
+		}
 
-    private function __construct()
-    {
-    }
+		private function setResponseCookie(
+			string $name,
+			string $msg
+		) : void {
+			$strippedMsg = preg_replace('/\s+/', ' ', $msg);
+			setcookie($name, $strippedMsg, strtotime("+" . RESPONSE_COOLDOWN), "/");
+		}
 
-    public static function getSingleton(): PhpUtils
-    {
-        if (self::$singleton === null)
-            self::$singleton = new PhpUtils();
-        return self::$singleton;
-    }
+		public function getResponseCookie(
+			string $name,
+			bool $unset
+		) : ?string {
+			$value = $_COOKIE[$name] ?? null;
+			if ($unset) {
+				self::unsetResponseCookie($name);
+			}
+			return $value;
+		}
 
-    public function onRedirectOk(?string $msg,
-        string $ref): void
-    {
-        self::setResponseCookie(RESPONSE_SUCCESS,$msg);
-        header("Location:$ref");
-    }
+		public function unsetResponseCookie(string $name) : void {
+			if (isset($_COOKIE[$name])) {
+				setcookie($name, "", 0, "/");
+			}
+		}
 
-    private function setResponseCookie(string $name,
-        string $msg): void
-    {
-        setcookie($name,$msg,strtotime("+" . RESPONSE_COOLDOWN),"/");
-    }
+		public function checkPhpInjection(string...$params) : bool {
+			$matches = function (array $args) : array {
+				$result = [];
+				foreach ($args as $arg) {
+					$result[] = preg_match(self::$PHP_INJECTION_REGEX_PATTERN, $arg);
+				}
+				return $result;
+			};
+			return in_array(true, $matches($params));
+		}
 
-    public function getResponseCookie(string $name,
-        bool $unset): ?string
-    {
-        $value = $_COOKIE[$name] ?? null;
+		public function onRedirectErr(
+			string $msg,
+			string $ref
+		) : void {
+			self::setResponseCookie(RESPONSE_FAILURE, $msg);
+			header("Location:$ref");
+		}
 
-        if ($unset)
-            self::unsetResponseCookie($name);
-
-        return $value;
-    }
-
-    public function unsetResponseCookie(string $name): void
-    {
-        if (isset($_COOKIE[$name]))
-            setcookie($name,"",0,"/");
-    }
-
-    public function checkPhpInjection(string...$params): bool
-    {
-        $matches = function (array $args): array {
-            $result = [];
-            foreach ($args as $arg)
-                $result[] = preg_match(self::$PHP_INJECTION_REGEX_PATTERN,$arg);
-            return $result;
-        };
-        return in_array(true,$matches($params));
-    }
-
-    public function onRedirectErr(string $msg,
-        string $ref): void
-    {
-        self::setResponseCookie(RESPONSE_FAILURE,$msg);
-        header("Location:$ref");
-    }
-
-    public function flushResponseCookies(): void
-    {
-        self::unsetResponseCookie(RESPONSE_SUCCESS);
-        self::unsetResponseCookie(RESPONSE_FAILURE);
-    }
-}
+		public function flushResponseCookies() : void {
+			self::unsetResponseCookie(RESPONSE_SUCCESS);
+			self::unsetResponseCookie(RESPONSE_FAILURE);
+		}
+	}
